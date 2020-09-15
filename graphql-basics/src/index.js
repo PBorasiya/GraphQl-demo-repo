4a3289da2ce4d7a60c9f89b7d0546ce1ac625095
PBorasiya <parixit.borasia@gmail.com>
2020-09-15 14:47:38 -0700

    Created a Post -> Author relationships in schema typeDefs

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,60 +1,144 @@
-import { GraphQLServer} from 'graphql-yoga'
+import {
+    GraphQLServer
+} from 'graphql-yoga'
 
+
+//Demo user data
+
+const users = [{
+            id: "1",
+            name: "Pranav",
+            email: "example.com",
+            age: 26
+        }, {
+            id: "2",
+            name: "Vishakha",
+            email: "vishoo.com",
+            age: 26
+        }, {
+            id: "3",
+            name: "Neel",
+            email: "neel.com",
+            age: 25
+        }, {
+            id: "4",
+            name: "Raja",
+            email: "raja.com",
+            age: 21
+        },{
+            id: "5",
+            name: "Jay",
+            email: "jay.com",
+            age: 23
+        }]
+
+//Demo post data
+
+const posts = [{
+                    id: '1',
+                    title: "GraphQl 101",
+                    body: "Welcome to the introductory course on GraphQL",
+                    published: true,
+                    author : '1'
+                },{
+                    id: '2',
+                    title: "Cricket 102",
+                    body: "Welcome to the advance course on Cricket",
+                    published: true,
+                    author : '1'
+                },{
+                    id: '3',
+                    title: "Music 103",
+                    body: "Welcome to the elite course on Music",
+                    published: false,
+                    author : '2'
+                },{
+                    id: '4',
+                    title: "GraphQl 104",
+                    body: "Welcome to the master course on GraphQL",
+                    published: false,
+                    author : '3'
+                }
+                ]
 const typeDefs = `
     type Query{
+        me : User!
+        post : Post!
+        users(query : String) : [User!]!
+        posts(query : String) : [Post!]!
+    }
+
+    type User{
         id : ID!
         name : String!
         age : Int!
-        employed : Boolean!
-        gpa : Float!
+        email : String!
+    }
+
+    type Post{
+        id : ID!
         title : String!
-        price : Float!
-        releaseYear :Int
-        rating : Float
-        inStock : Boolean!
+        body : String!
+        published : Boolean!
+        author : User!
     }
 `
 
 const resolvers = {
-    Query : {
-       id(){
-           return 'abc1234'
-       },
-       name(){
-           return 'Pranav Borasiya'
-       },
-       age(){
-           return 26
-       },
-       employed(){
-           return true
-       },
-       gpa(){
-            return 3.26
-       },
-       title(){
-           return 'Pranav is the best product'
-       },
-       price(){
-           return 150000.50
-       },
-       releaseYear(){
-           return 2018
-       },
-       rating(){
-           return 5
-       },
-       inStock(){
-            return true
-       }
-    }
+        Query: {
+            me() {
+                return {
+                    id: "123098",
+                    name: "Pranav",
+                    email: "example.com",
+                    age: 28
+                }
+            },
+            post() {
+                return {
+                    id: '12kejej12',
+                    title: "GraphQl 101",
+                    body: "Welcome to the introductory course on GraphQL",
+                    published: true
+                }
+            },
+            users(parent, args, ctx, info) {
+
+                if(!args.query){
+                    return users
+                }
+
+                return users.filter((user) => {
+                       return user.name.toLowerCase().includes(args.query.toLowerCase())
+                })
+            },
+            posts(parent, args, ctx, info){
+                if(!args.query){
+                    return posts
+                }
+
+                return posts.filter((post) => {
+                    const isTitleMatch =  post.title.toLowerCase().includes(args.query.toLowerCase())
+                    const isBodyMatch =  post.body.toLowerCase().includes(args.query.toLowerCase())
+
+                    return isTitleMatch || isBodyMatch
+                })
+            }
+        },
+        Post : {
+            author(parent,args,ctx,info){
+                return users.find((user) => {
+                    return user.id === parent.author
+                })
+            }
+        }
 }
 
-const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
+        const server = new GraphQLServer({
+            typeDefs,
+            resolvers
+        })
 
-server.start(() =>{
-    console.log('Server is running!')
-})
+        server.start(() => {
+            console.log('Server is running!')
+        })
