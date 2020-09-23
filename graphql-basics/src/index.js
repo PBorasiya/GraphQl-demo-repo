e211913be29b53b1c1b3ed9faeb04be4c520ad18
PBorasiya <parixit.borasia@gmail.com>
2020-09-23 16:03:03 -0700

    Created schema.graphql to refactor code into more manageable project structure

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -81,72 +81,8 @@ let comments = [{
             post : '1'          
 }]
 
-//input type must only include scalar type and cannot have any object or complex type.
-const typeDefs = `
-    type Query{
-        me : User!
-        post : Post!
-        users(query : String) : [User!]!
-        posts(query : String) : [Post!]!
-        comments : [Comment!]!
-    }
 
-    type Mutation {
-        createUser( data : CreateUserInput ) : User!
-        deleteUser(id : ID!) : User!
 
-        createPost( data : CreatePostInput ) : Post!
-        deletePost( id : ID! ) : Post!
-
-        createComment( data : CreateCommentInput ) : Comment!
-        deleteComment( id : ID!) : Comment!
-    } 
-
-    
-    input CreateUserInput{
-        name : String!
-        email : String!
-        age : Int!
-    }
-
-    input CreatePostInput{
-        title : String!
-        body : String!
-        published : Boolean!
-        author : ID!
-    }
-
-    input CreateCommentInput{
-        text : String!
-        author : ID!
-        post : ID!
-    }
-
-    type User{
-        id : ID!
-        name : String!
-        age : Int!
-        email : String!
-        posts : [Post!]!
-        comments : [Comment!]!
-    }
-
-    type Post{
-        id : ID!
-        title : String!
-        body : String!
-        published : Boolean!
-        author : User!
-        comments : [Comment!]!
-    }
-
-    type Comment{
-        id : ID!
-        text : String!
-        author : User!
-        post : Post!
-    }
-`
 
 const resolvers = {
         Query: {
@@ -341,7 +277,7 @@ const resolvers = {
 }
 
         const server = new GraphQLServer({
-            typeDefs,
+            typeDefs : './src/schema.graphql',
             resolvers
         })
 
