7a4087d7cd1c0795aa9250a2cd07b4860096ccd8
PBorasiya <parixit.borasia@gmail.com>
2020-09-23 17:06:29 -0700

    Created update user mutation.

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -42,6 +42,33 @@ const Mutation =  {
 
         return deletedUsers[0]
     },
+    updateUser(parent, args , {db}, info){
+        const user = db.users.find((user) => user.id === args.id)
+
+        if(!user){
+            throw new Error('User not found.')
+        }
+
+        if(typeof args.data.email === 'string'){
+            const emailTaken = db.users.some((user) => user.email === args.data.email)
+
+            if(emailTaken){
+                throw new Error('Email in use.')
+            }
+
+            user.email = args.data.email
+        }
+
+        if(typeof args.data.name === 'string'){
+            user.name = args.data.name
+        }
+
+        if(typeof args.data.age !== 'undefined'){
+            user.age = args.data.age
+        }
+
+        return user
+    },
     createPost(parent, args, { db }, info){
         const userExists = db.users.some((user) => user.id === args.data.author)
 
