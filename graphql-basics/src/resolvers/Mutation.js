97e9f3f96b1f04933cbf6b7458398668deedb235
PBorasiya <parixit.borasia@gmail.com>
2020-09-25 14:35:46 -0700

    Created subscription for creating, updating and deleting comment

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -182,23 +182,35 @@ const Mutation =  {
         }
 
         db.comments.push(comment)
-        pubsub.publish(`comment ${args.data.post}`, {comment})
+        pubsub.publish(`comment ${args.data.post}`, {
+            comment :{
+                mutation : 'CREATED',
+                data : comment
+            }
+        })
 
         return comment
     },
-    deleteComment(parent, args, {db}, info){
+    deleteComment(parent, args, {db, pubsub}, info){
         const commentIndex = db.comments.findIndex((comment) => comment.id === args.id)
 
         if(commentIndex === -1){
             throw new Error('Comment does not exist')
         }
 
-        const deletedComments = db.comments.splice(commentIndex,1)
+        const [deletedComment] = db.comments.splice(commentIndex,1)
 
-        return deletedComments[0]
+        pubsub.publish(`comment ${deletedComment.post}`, {
+            comment : {
+                mutation : 'DELETED',
+                data : deletedComment
+            }
+        })
+
+        return deletedComment
     },
-    updateComment(parent, args, {db}, info){
-        const comment = db.comments.find((Comment) => Comment.id === args.id)
+    updateComment(parent, args, {db, pubsub}, info){
+        const comment = db.comments.find((comment) => comment.id === args.id)
 
         if(!comment){
             throw new Error('Comment does not exist.')
@@ -207,6 +219,13 @@ const Mutation =  {
         if(typeof args.data.text === 'string'){
             comment.text = args.data.text
         }
+
+        pubsub.publish(`comment ${comment.post}`, {
+            comment : {
+                mutation : 'UPDATED',
+                data : comment
+            }
+        })
 
         return comment
     }
