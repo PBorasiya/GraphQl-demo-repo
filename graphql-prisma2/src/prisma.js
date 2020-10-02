cbe08dfcb1b4f965a9ca581778ba6293f9aaf88b
PBorasiya <parixit.borasia@gmail.com>
2020-10-01 18:11:36 -0700

    trying to use prisma binding

--- graphql-prisma2/src/prisma.js
+++ graphql-prisma2/src/prisma.js
@@ -2,5 +2,9 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs : 'src/generated/prisma.graphql',
-    endpoint : 'localhost:4466'
+    endpoint : 'http://localhost:4466'
+})
+
+prisma.query.users(null, '{id name email}').then((data)=>{
+    console.log(data)
 })
