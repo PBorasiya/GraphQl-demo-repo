7de6846280d8770f7a5d361e3e2c6afbc5debae5
PBorasiya <parixit.borasia@gmail.com>
2020-10-01 18:40:40 -0700

    Used prisma-binding to update post

--- graphql-prisma2/src/prisma.js
+++ graphql-prisma2/src/prisma.js
@@ -13,18 +13,32 @@ const prisma = new Prisma({
 //     console.log(JSON.stringify(data,undefined,4))
 // })
 
-prisma.mutation.createPost({
-    data : {
-        title : "new graphqlpost from prisma-binding",
-        body : "ajakkjahfdhfjasjfsafdsaddadjshfahn bjbfhakfhajsd adka dkaxdhakjh  hjfhjkrhanxa",
-        published : true,
-        author : {
-            connect : {
-                id : "ckfrhrcjt00220779vdpgwtmp"
-            }
-        }
+// prisma.mutation.createPost({
+//     data : {
+//         title : "new graphqlpost from prisma-binding",
+//         body : "ajakkjahfdhfjasjfsafdsaddadjshfahn bjbfhakfhajsd adka dkaxdhakjh  hjfhjkrhanxa",
+//         published : true,
+//         author : {
+//             connect : {
+//                 id : "ckfrhrcjt00220779vdpgwtmp"
+//             }
+//         }
+//     }
+// },'{id title body author{id name email}}').then((data) =>{
+//     console.log(JSON.stringify(data,undefined,4))
+//     return prisma.query.users(null, '{id name email posts{ id body title published}}')
+// }).then((data)=>{
+//     console.log(JSON.stringify(data,undefined,4))
+// })
+
+prisma.mutation.updatePost({
+    data:{
+        published: false
+    },
+    where : {
+        id : "ckfrklqvb00430779gpdua8pk"
     }
-},'{id title body author{id name email}}').then((data) =>{
+},'{id title body author{id name email}}').then((data)=>{
     console.log(JSON.stringify(data,undefined,4))
     return prisma.query.users(null, '{id name email posts{ id body title published}}')
 }).then((data)=>{
