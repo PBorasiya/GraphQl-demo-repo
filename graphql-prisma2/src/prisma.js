b72a1faca7aad1e674bfb5f36e3ddce6379f3e78
PBorasiya <parixit.borasia@gmail.com>
2020-10-02 17:14:00 -0700

    Repurposed the code to use async await rather than promise chaining.

--- graphql-prisma2/src/prisma.js
+++ graphql-prisma2/src/prisma.js
@@ -28,29 +28,50 @@ const prisma = new Prisma({
 //     console.log(JSON.stringify(data,undefined,4))
 // })
 
-const createPostForUser = async(authorId, data) =>{
-    const post = await prisma.mutation.createPost({
-        data : {
-            ...data,
-            author : {
-                connect : {
-                    id : authorId
-                }
-            }
-        }
-    }, '{ id }')
-    const user = await prisma.query.user({
+// const createPostForUser = async(authorId, data) =>{
+//     const post = await prisma.mutation.createPost({
+//         data : {
+//             ...data,
+//             author : {
+//                 connect : {
+//                     id : authorId
+//                 }
+//             }
+//         }
+//     }, '{ id }')
+//     const user = await prisma.query.user({
+//         where : {
+//             id : authorId
+//         }
+//     }, '{id name email posts{ id title body published}}')
+//     return user
+// }
+
+// createPostForUser('ckfrhqmxl00110779i1zelp7z', {
+//     title : 'How to get your man listening to you',
+//     body : 'They do listen, they just ignore you for sports. so wait for game to be over then talk',
+//     published : true
+// }).then((user)=>{
+//     console.log(JSON.stringify(user,undefined,4))
+// })
+
+const updatePostForUser = async(postId, data) =>{
+    const post = await prisma.mutation.updatePost({
         where : {
-            id : authorId
+            id : postId
+        },
+        data
+    }, '{author {id name email}}')
+    const user = await prisma.query.user({
+        where:{
+            id : post.author.id
         }
-    }, '{id name email posts{ id title body published}}')
+    }, '{id name email posts {id title published}}')
     return user
 }
 
-createPostForUser('ckfrhqmxl00110779i1zelp7z', {
-    title : 'How to get your man listening to you',
-    body : 'They do listen, they just ignore you for sports. so wait for game to be over then talk',
-    published : true
+updatePostForUser('ckfswyesw001c0879rfboeton', {
+    "title": "How to get your man listening to you 3.0"
 }).then((user)=>{
     console.log(JSON.stringify(user,undefined,4))
 })
