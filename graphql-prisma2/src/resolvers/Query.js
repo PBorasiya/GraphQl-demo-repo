9acb375222fd1be8e3d96a1818eb8256cb5f6e07
PBorasiya <parixit.borasia@gmail.com>
2020-10-06 16:46:07 -0700

    Refactored query resolvers to deal with database data rather than static data using prisma

--- graphql-prisma2/src/resolvers/Query.js
+++ graphql-prisma2/src/resolvers/Query.js
@@ -1,45 +1,36 @@
 const Query = {
-    me() {
-        return {
-            id: "123098",
-            name: "Pranav",
-            email: "example.com",
-            age: 28
-        }
-    },
-    post() {
-        return {
-            id: '12kejej12',
-            title: "GraphQl 101",
-            body: "Welcome to the introductory course on GraphQL",
-            published: true
-        }
-    },
-    users(parent, args, { db }, info) {
+    
+    users(parent, args, { prisma }, info) {
 
-        if(!args.query){
-            return db.users
-        }
+        return prisma.query.users(null, info)
 
-        return db.users.filter((user) => {
-               return user.name.toLowerCase().includes(args.query.toLowerCase())
-        })
+        // if(!args.query){
+        //     return db.users
+        // }
+
+        // return db.users.filter((user) => {
+        //        return user.name.toLowerCase().includes(args.query.toLowerCase())
+        // })
+
+
     },
     
-    posts(parent, args, { db }, info){
-        if(!args.query){
-            return db.posts
-        }
+    posts(parent, args, { prisma }, info){
 
-        return db.posts.filter((post) => {
-            const isTitleMatch =  post.title.toLowerCase().includes(args.query.toLowerCase())
-            const isBodyMatch =  post.body.toLowerCase().includes(args.query.toLowerCase())
+        return prisma.query.posts(null, info)
+        // if(!args.query){
+        //     return db.posts
+        // }
 
-            return isTitleMatch || isBodyMatch
-        })
+        // return db.posts.filter((post) => {
+        //     const isTitleMatch =  post.title.toLowerCase().includes(args.query.toLowerCase())
+        //     const isBodyMatch =  post.body.toLowerCase().includes(args.query.toLowerCase())
+
+        //     return isTitleMatch || isBodyMatch
+        // })
     },
-    comments(parent,args,{ db },info){
-        return db.comments
+    comments(parent,args,{ prisma },info){
+        return prisma.query.comments(null, info)
     }
 }
 
