81cba42c57f9632804112c84130348d8bc680dbf
PBorasiya <parixit.borasia@gmail.com>
2020-10-06 17:05:30 -0700

    Added support for opArgs in post and users query.

--- graphql-prisma2/src/resolvers/Query.js
+++ graphql-prisma2/src/resolvers/Query.js
@@ -2,22 +2,37 @@ const Query = {
     
     users(parent, args, { prisma }, info) {
 
-        return prisma.query.users(null, info)
+        const opArgs = {}
 
-        // if(!args.query){
-        //     return db.users
-        // }
+        if(args.query){
+            opArgs.where={
+                OR :[{
+                    name_contains : args.query
+                },{
+                    email_contains : args.query
+                }]
+            }
+        }
 
-        // return db.users.filter((user) => {
-        //        return user.name.toLowerCase().includes(args.query.toLowerCase())
-        // })
-
+        return prisma.query.users(opArgs, info)
 
     },
     
     posts(parent, args, { prisma }, info){
 
-        return prisma.query.posts(null, info)
+        const opArgs = {}
+
+        if(args.query){
+            opArgs.where={
+                OR : [{
+                    title_contains : args.query
+                },{
+                    body_contains: args.query
+                }]
+            }
+        }
+
+        return prisma.query.posts(opArgs, info)
         // if(!args.query){
         //     return db.posts
         // }
