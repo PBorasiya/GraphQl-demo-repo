b460f2638028ca7f61ae5037c69b07ec1c521937
PBorasiya <parixit.borasia@gmail.com>
2020-10-09 15:29:59 -0700

    Added prisma to updateUser mutation.

--- graphql-prisma2/src/resolvers/Mutation.js
+++ graphql-prisma2/src/resolvers/Mutation.js
@@ -28,50 +28,17 @@ const Mutation =  {
             where :{
                 id : args.id
         }}, info)
-        // const deletedUsers = db.users.splice(userIndex,1,)
-
-        // db.posts = db.posts.filter((post) => {
-        //     const match = post.author === args.id
-
-        //     if(match){
-        //         db.comments = db.comments.filter((comment) => {
-        //             return Comment.post !== post.id
-        //         })
-        //     }
-
-        //     db.comments = db.comments.filter((comment) => comment.author !== args.id)
-
-        //     return !match
-        // })
-
-        // return deletedUsers[0]
+        
     },
-    updateUser(parent, args , {db}, info){
-        const user = db.users.find((user) => user.id === args.id)
+    async updateUser(parent, args , { prisma }, info){
 
-        if(!user){
-            throw new Error('User not found.')
-        }
-
-        if(typeof args.data.email === 'string'){
-            const emailTaken = db.users.some((user) => user.email === args.data.email)
-
-            if(emailTaken){
-                throw new Error('Email in use.')
-            }
-
-            user.email = args.data.email
-        }
-
-        if(typeof args.data.name === 'string'){
-            user.name = args.data.name
-        }
-
-        if(typeof args.data.age !== 'undefined'){
-            user.age = args.data.age
-        }
-
-        return user
+        return prisma.mutation.updateUser({
+            where:{
+                id : args.id
+            },
+            data : args.data
+        },info)
+        
     },
     createPost(parent, args, { db , pubsub}, info){
         const userExists = db.users.some((user) => user.id === args.data.author)
