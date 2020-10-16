75515dc544cdc8ed046790031a47d85892263308
PBorasiya <parixit.borasia@gmail.com>
2020-10-16 15:54:39 -0700

    Locking down personal data such as email to anauthorized users.

--- graphql-prisma2/src/resolvers/User.js
+++ graphql-prisma2/src/resolvers/User.js
@@ -1,6 +1,16 @@
+import getUserId from './../utils/getuserid'
+
 //User object gets related data from the original call and is neede to make Author-> Post and Author-> comments relationship
 const User = {
-    
+    email(parent,args,{request},info){
+        const userId = getUserId(request,false)
+
+        if(userId && userId === parent.id){
+            return parent.email
+        }else{
+            return null
+        }
+    }
 }
 
 export { User as default }
