056a48d8f45716fdfa2ca670799a9b3fd8c7e279
PBorasiya <parixit.borasia@gmail.com>
2020-10-16 16:20:57 -0700

    Added fragmentReplacements from prisma-binding to fetch custom fragments that allows to specify the field to run correctlu.

--- graphql-prisma2/src/index.js
+++ graphql-prisma2/src/index.js
@@ -1,25 +1,14 @@
 import {GraphQLServer,PubSub} from 'graphql-yoga'
 import db from './db'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
-import Subscription from './resolvers/Subscription'
 import prisma from './prisma'
+import {resolvers,fragmentReplacements} from './resolvers/index'
 
 const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs : './src/schema.graphql',
-    resolvers : {
-        Query,
-        Mutation,
-        User,
-        Post,
-        Comment,
-        Subscription
-    },
+    resolvers,
+    fragmentReplacements,
     context(request){
         return{
             db,
--- graphql-prisma2/src/resolvers/User.js
+++ graphql-prisma2/src/resolvers/User.js
@@ -2,13 +2,16 @@ import getUserId from './../utils/getuserid'
 
 //User object gets related data from the original call and is neede to make Author-> Post and Author-> comments relationship
 const User = {
-    email(parent,args,{request},info){
-        const userId = getUserId(request,false)
-
-        if(userId && userId === parent.id){
-            return parent.email
-        }else{
-            return null
+    email:{
+        fragment: 'fragment userId on User { id }',
+        resolve(parent,args,{request},info){
+            const userId = getUserId(request,false)
+    
+            if(userId && userId === parent.id){
+                return parent.email
+            }else{
+                return null
+            }
         }
     }
 }
