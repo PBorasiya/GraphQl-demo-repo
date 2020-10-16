fd123079f8bd3e532b8c11920c710694b84c4c03
PBorasiya <parixit.borasia@gmail.com>
2020-10-16 16:32:41 -0700

    Solved edge case scenario for queries where you can't search for users directly through their email.

--- graphql-prisma2/src/resolvers/Query.js
+++ graphql-prisma2/src/resolvers/Query.js
@@ -10,8 +10,6 @@ const Query = {
             opArgs.where={
                 OR :[{
                     name_contains : args.query
-                },{
-                    email_contains : args.query
                 }]
             }
         }
