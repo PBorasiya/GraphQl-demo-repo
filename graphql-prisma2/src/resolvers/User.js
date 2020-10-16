9d8aa5ac460078fa058c387101ce09c94b28edec
PBorasiya <parixit.borasia@gmail.com>
2020-10-16 16:29:55 -0700

    made posts fragment to make sure only published posts are shown for another users.

--- graphql-prisma2/src/resolvers/User.js
+++ graphql-prisma2/src/resolvers/User.js
@@ -1,7 +1,21 @@
 import getUserId from './../utils/getuserid'
 
+
 //User object gets related data from the original call and is neede to make Author-> Post and Author-> comments relationship
 const User = {
+    posts:{
+        fragment : 'fragment userId on User { id }',
+        resolve(parent, args, {prisma}, info){
+            return prisma.query.posts({
+                where :{
+                    published : true,
+                    author : {
+                        id : parent.id
+                    }
+                }
+            })
+        }
+    },
     email:{
         fragment: 'fragment userId on User { id }',
         resolve(parent,args,{request},info){
